c2771b0d00bc93993f42d78740f5dd338c4b6ff2
LuisFernandoHedzHedz005 <luisfdohernandez005@gmail.com>
2024-11-20 23:53:56 -0600

    Modificando database config

--- config/db.js
+++ config/db.js
@@ -4,14 +4,14 @@ const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
     password: '123456',
-    database: 'registroestudiante',
+    database: 'RegistroEstudiante',
     multipleStatements: true, 
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
 });
 
-const query = async (sql, params) => {
+const query = async (sql, params) => {S
     try {
         const [results] = await pool.execute(sql, params);
         return results;
